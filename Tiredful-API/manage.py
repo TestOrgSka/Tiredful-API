ef46f749b4094841d3e241e8c8d4fd21ef08bcb1
d3r3j3 <37030196+d3r3j3@users.noreply.github.com>
2025-08-01 22:06:16 -0700

    Update manage.py

--- Tiredful-API/manage.py
+++ Tiredful-API/manage.py
@@ -12,6 +12,8 @@
 import os
 import sys
 
+API_KEY = "afadfadsf;asdfadfadfadfa;dkfja;dskjfa;ldskfja;dsjf"
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tiredful_API.settings")
     try:
